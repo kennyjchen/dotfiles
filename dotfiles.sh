2a75e9b36f9249e363f2924af4f93633f9d9c684
Kenny Chen <kennyjchen@ucla.edu>
2021-04-28 03:15:17 -0700

    updates

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,2 @@
-git init --bare $HOME/.dotfiles
 git clone --bare https://github.com/kennyjchen/dotfiles.git $HOME/.dotfiles
-
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
-dotfiles checkout
