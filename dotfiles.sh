50a7f5559805fd86fb7005390d478ddd27d1588a
Kenny Chen <kennyjchen@ucla.edu>
2021-05-02 15:20:30 -0700

    install universal ctags for vim

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,2 +1,17 @@
 git clone --bare git@github.com:kennyjchen/dotfiles.git $HOME/.dotfiles
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+
+# Install ThirdParty Software
+mkdir ~/Software/
+mkdir ~/Software/ThirdParty/
+cd ~/Software/ThirdParty
+
+# Universal Ctags
+git clone https://github.com/universal-ctags/ctags.git
+cd ./ctags
+./autogen.sh
+./configure --prefix=/home/kjchen/.local
+make
+make install
+
+# Virtualenv
